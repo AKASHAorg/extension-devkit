ca94499753820cc3e02954d22b2341c6ffc1545a
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2025-03-14 16:11:41 +0100

    refactor: update component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,8 @@
-/*@ts-ignore-next-line*/
-import logoWhite from '../assets/devkit-logo-white.png?inline';
-/*@ts-ignore-next-line*/
-import logoBlack from '../assets/devkit-logo-black.png?inline';
-
-import { Button } from './ui/button';
 import { useAkashaStore } from '@akashaorg/ui-core-hooks';
 import { useGetProfileByDidQuery } from '@akashaorg/ui-core-hooks/lib/generated/apollo';
 import { selectProfileData } from '@akashaorg/ui-core-hooks/lib/selectors/get-profile-by-did-query';
-import { Card, CardContent, CardFooter, CardHeader } from './ui/card';
-import { Image, ImageRoot } from './ui/image';
+import { Button } from './ui/button';
+import { Card, CardContent, CardHeader } from './ui/card';
 import { Typography } from './ui/typography';
 
 const App = () => {
@@ -17,7 +11,7 @@ const App = () => {
   } = useAkashaStore();
 
   const handleAuth = () => {
-    // @todo
+    // some auth logic
   };
 
   const profileDataReq = useGetProfileByDidQuery({
@@ -31,28 +25,6 @@ const App = () => {
 
   return (
     <div>
-      <div className="p-4 rounded-2xl text-black dark:text-white justify-center border-none">
-        <div className="gap-4 flex flex-col">
-          <div>
-            <ImageRoot className="flex justify-center opacity-60">
-              <Image src={logoWhite} className="hidden dark:block w-2/3" />
-              <Image src={logoBlack} className="dark:hidden w-2/3" />
-            </ImageRoot>
-          </div>
-          <div className="w-full flex">
-            <div className="flex justify-center w-full">
-              Documentation:
-              <a
-                className="ml-2 text-indigo-400"
-                href="https://docs.akasha.world/devkit"
-                target="_blank"
-              >
-                docs.akasha.world/devkit
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
       {isAuthenticating && <div>User is authenticating...</div>}
 
       {!isAuthenticating && (
@@ -70,11 +42,11 @@ const App = () => {
               </CardHeader>
               <CardContent style={{ flexDirection: 'column', gap: '0.5rem' }}>
                 <Typography>
-                  You have created your profile on: {profileCreationDate.getDay()}/
+                  Your profile was created on: {profileCreationDate.getDay()}/
                   {profileCreationDate.getMonth()}/{profileCreationDate.getFullYear()}
                 </Typography>
                 <Typography>
-                  You have authenticated with the Decentralized IDentity: {userProfile?.did.id}
+                  Your Decentralized IDentity is: {userProfile?.did.id}
                 </Typography>
               </CardContent>
             </Card>
