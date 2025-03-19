6f5a299ddd063668b3d41334915019daef7603ca
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-03-19 17:30:30 +0100

    chore(): remove temo fix

--- src/extensions/poll-block/index.tsx
+++ src/extensions/poll-block/index.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { withProviders } from '@akashaorg/ui-core-hooks';
 
-import { BlockInstanceMethods, ContentBlockRootProps } from '@akashaorg/typings/lib/ui';
+import {
+  BlockInstanceMethods,
+  ContentBlockModes,
+  ContentBlockRootProps,
+} from '@akashaorg/typings/lib/ui';
 import { PollBlock } from './poll-block';
 import { PollReadonlyBlock } from './poll-readonly-block';
-
-const ContentBlockModes = {
-  EDIT: 'edit-mode',
-  READONLY: 'read-only-mode',
-};
 
 const PollBlockExtension = (
   props: ContentBlockRootProps & { blockRef?: React.RefObject<BlockInstanceMethods> },
